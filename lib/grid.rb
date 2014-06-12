9d6f13c1a9bd2ffa1d2906f1d99508e48a82c53c
gomenasaai <contramundee@gmail.com>
2014-06-12 23:24:50 +0100

    removed some comments

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,12 +1,28 @@
+require_relative 'cell'
+
 class Grid
 
-	def initialize
+	def initialize initial_values
 		@cells = #create grid of 81 cells
 	end
 
 	attr_reader :cells
 
 	def solve
+		outstanding_before, looping = SIZE, false
+		while !solved? && !looping
+			try_to_solve
+			outstanding = @cells.count {|c| c.solved?}
+			looping = outstanding_before == looping
+			outstanding_before = outstanding
+	end
+
+	def solved?
+		
+	end
+
+	def inspect
+		
 	end
 
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -1,5 +1,33 @@
 require 'grid'
 
 describe Grid do
+
+	let(:puzzle) {'015003002000100906270068430490002017501040380003905000900081040860070025037204600'}
+	let(:grid) {Grid.new(puzzle)}
+
+	context 'initialization' do
+
+		it 'should have 81 cells' do
+
+		end
+
+		it 'should have an unsolved first cell' do
+
+		end
+
+		it 'should have a solved second cell with value of 1' do
+
+		end
+
+	end
+
+	context 'solving sudoku' do
+		it 'can solve a puzzle' do
+			expect(grid.solved?).to be_false
+			grid.solve
+			expect(grid.solved?).to be_true
+			expect(grid.to_s).to eq('615493872348127956279568431496832517521746389783915264952681743864379125137254698')
+		end
+	end
 	
 end
